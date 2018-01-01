ef87a3f2846d0fb67220a9293d903f4af00316f2
Aditya Garg <adityagarg97@gmail.com>
2018-01-01 18:55:26 +0530

    Making changes
    Signed-off-by: Aditya Garg <adityagarg97@gmail.com>

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path=require("path")
 const table=require("./db")
 const app=express()
-const port=process.env.port||1111
+const port=process.env.PORT||1111
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,"/public")))
