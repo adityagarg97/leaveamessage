b1db3455a4617fa49c09a6715125ffb7d1696be4
Aditya Garg <adityagarg97@gmail.com>
2018-01-06 17:15:54 +0530

    Removing db dependency
    Signed-off-by: Aditya Garg <adityagarg97@gmail.com>

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 const express = require("express")
 const path=require("path")
-const table=require("./db")
 const app=express()
 const port=process.env.PORT||1111
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,"/public")))
+table=[]
 app.post("/",(req,res)=>{
     if(req.body.name===null || req.body.email===null){
         return res.status(403).send({
             error: "Invalid Data"
         })
     }
-    table.create({
+    table.push({
         name:req.body.name,
         email:req.body.email,
         message:req.body.message
-    }).then((data)=>{
-        res.status(201).send(data)
-    }).catch((err)=>{
-        res.status(501).send({
-            error:"Error sending message"
-        })
     })
+    res.send(table)
+})
+app.get("/message",(req,res)=>{
+    res.send(table)
 })
 app.listen(1111,()=>console.log("Server started at "+port))
